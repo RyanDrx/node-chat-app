5d93e310be75b164b2e5a1bb000339021885f4ea
Ryand <Ryand@hptest.info>
2017-03-13 17:10:43 -0700

    add port variable for heroku

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(publicPath));
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`server is up on port ${port}`);
 })
 
