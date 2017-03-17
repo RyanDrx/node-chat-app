a92ca47078a9c8404f18534da777ef3209d4350d
Ryand <Ryand@hptest.info>
2017-03-17 15:53:23 -0700

    emit new message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,13 @@
 var socket = io();
 
 socket.on('connect', function () {
-    console.log('Connected to Server');
-
-    socket.emit('createMessage', {
-        from: "woo-lahoo!",
-        text: "Hello Friend"
-    })
+    console.log('Connected to server');
 });
 
 socket.on('disconnect', function () {
-    console.log('Disconnected from Server');
+    console.log('Disconnected from server');
 });
 
 socket.on('newMessage', function (message) {
-    console.log('New Message', message);
+    console.log('newMessage', message);
 });
-
